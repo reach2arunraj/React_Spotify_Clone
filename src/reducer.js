b8b80d2d17a8ee0c83dafe85e54bb32dbf6febb1
reach2arunraj <60217918+reach2arunraj@users.noreply.github.com>
2020-08-30 13:36:37 +0530

    styling the options components and pulling playlist from  spotify API

--- src/reducer.js
+++ src/reducer.js
@@ -3,7 +3,7 @@ export const initialState = {
     playlists: [],
     playing: false,
     item: null,
-    token: "BQCYgeUmSV75moRvOxdGcD1k-pPC66q-9mWDvej5BFFn5M9ga0p1FNkNeTAG7ZqDKmgrRXpYffKOveN28vgaNqE8pBvVqNn6ZVAd4F12szxwmLwWpI-gcKlGDLpklujQo7NqEC5ShWvherJ7IMrti0urmDz-jtUu8y18CFovAYtaQg8Z"
+    // token: "BQCYgeUmSV75moRvOxdGcD1k-pPC66q-9mWDvej5BFFn5M9ga0p1FNkNeTAG7ZqDKmgrRXpYffKOveN28vgaNqE8pBvVqNn6ZVAd4F12szxwmLwWpI-gcKlGDLpklujQo7NqEC5ShWvherJ7IMrti0urmDz-jtUu8y18CFovAYtaQg8Z"
 }
 
 const reducer = (state, action) => {
@@ -17,6 +17,10 @@ const reducer = (state, action) => {
         case "SET_TOKEN":
             return {
                 ...state, token : action.token
+            }
+        case "SET_PLAYLISTS":
+            return {
+                ...state, playlists : action.playlists
             }
         default:
             return state;    
